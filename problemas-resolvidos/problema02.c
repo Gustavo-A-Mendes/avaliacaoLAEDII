cbb829f84a50d216d78567969b67a1dc5c7e78f3
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2024-03-03 15:39:05 -0300

    att

--- problemas-resolvidos/problema02.c
+++ problemas-resolvidos/problema02.c
@@ -1,14 +1,16 @@
+/* Programa que transcreve os caracteres de 0 a 127
+   no códico ASCII, de 10 em 10
+*/
 #include <stdio.h>
 
 int main() {
     int i;
-
     printf("Caracteres ASCII de 0 a 127:\n");
 
     // Imprimir caracteres ASCII de 0 a 127
     for (i = 0; i < 128; i++) {
         // Imprimir o caractere
-        printf("%c ", (char)i);
+        printf("%c ", i);
 
         // Imprimir uma nova linha a cada 10 caracteres
         if ((i + 1) % 10 == 0) {
